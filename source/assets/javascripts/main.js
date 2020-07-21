ab4c5a6ded9eaa3f873f6e754964a9a1cc2c8a34
Jay Perry <j@jayperryworks.com>
2020-07-21 12:24:11 -0400

    [#164] add JS scrolling animation for anchor links

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,6 +1,15 @@
+import SmoothScroll from 'smooth-scroll'
 import flyout from './modules/flyout.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   // set up flyout toggle buttons
   flyout('data-js-toggle')
+})
+
+// -> need to declare this variable per SmoothScroll's API
+// -> https://github.com/cferdinandi/smooth-scroll
+// eslint-disable-next-line no-unused-vars
+let scroll = new SmoothScroll('[data-js-scroll], a[href*="#"]', {
+  speed: 600,
+  easing: 'easeInOutCubic'
 })

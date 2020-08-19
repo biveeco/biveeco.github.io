a918fab659e383a87c7972bba65a36618b671243
Jay Perry <j@jayperryworks.com>
2020-08-19 16:57:20 -0400

    clean up css for animation and positioning #175 kill inline hacky styles and add utility classes to create the homepage layout

--- source/assets/javascripts/modules/change-on-intersect.js
+++ source/assets/javascripts/modules/change-on-intersect.js
@@ -2,7 +2,7 @@ export default function (targets, callback, options = { threshold: 0.5 }) {
 	if ('IntersectionObserver' in window) {
 		const targetList = Array.from(document.querySelectorAll(targets))
 
-		if (targetList.length > 1) {
+		if (targetList.length > 0) {
 			const targetObserver = new IntersectionObserver((entries, observer) => {
 				entries.forEach((entry) => {
 					entry.isIntersecting && callback(entry)
